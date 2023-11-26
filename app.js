73a92d0fc180fd358f37567448a064b8010c4d8a
opoberezh <opoberezh.2015@gmail.com>
2023-11-26 20:51:43 +0100

    completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("node:path");
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 require("dotenv").config();
@@ -12,6 +13,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/avatars", express.static(path.join(__dirname, "public", "avatars")));
 
 app.use("/users", authRouter);
 app.use("/contacts", contactsRouter);
